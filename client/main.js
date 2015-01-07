9a268199a0cfd05e314c98e9bbf3a5f63608c733
Timo Suomela <timo.suomela@reaktor.fi>
2015-01-07 10:09:52 +0200

    Add a circle that when clicked turns green

--- client/main.js
+++ client/main.js
@@ -4,5 +4,20 @@ var $ = require('jquery');
 $(document).ready(init);
 
 function init() {
-  $('<h1>').text("LAGUNS").appendTo($('body'));
+  var $body = $('body');
+  $body
+    .append(title())
+    .append(circle());
+}
+
+function title() {
+  return $('<h1>').text("LAGUNS");
+}
+
+function circle() {
+  var $circle = $('<div>', { 'class': 'circle circle-white' });
+  $circle.on('click', function() {
+    $circle.removeClass('circle-white').addClass('circle-green')
+  });
+  return $circle;
 }
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var browserify = require('browserify-middleware');
+var lessify = require('less-middleware');
 var express = require('express');
 var app = express();
+var publicDir = __dirname + '/public';
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(lessify(publicDir));
+app.use(express.static(publicDir));
 app.use('/js/bundle.js', browserify('./client/main.js'));
 app.listen(app.get('port'), function() {
   console.log("Server is running at localhost:" + app.get('port'));
