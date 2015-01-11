c4f7d207dc0c67beea6e68f620323a3ee218b993
Timo Suomela <timo.suomela@reaktor.fi>
2015-01-11 17:20:55 +0200

    Add a block with title and four circles for each excercise

--- client/main.js
+++ client/main.js
@@ -1,23 +1,32 @@
 var Bacon = require('baconjs');
 var $ = require('jquery');
+require('normalize-css');
 
 $(document).ready(init);
 
 function init() {
-  var $body = $('body');
-  $body
-    .append(title())
-    .append(circle());
+  ['Reverse', 'Incline', 'Single', 'Curl', 'Triceps', 'Shrugs', 'Calf']
+    .map(block)
+    .reduce(append, $('<div>', { 'class': 'content' }))
+    .appendTo($('body'));
 }
 
-function title() {
-  return $('<h1>').text("LAGUNS");
+function block(title) {
+  return $('<h2>').text(title).add(circles());
+}
+
+function circles() {
+  return [1, 2, 3, 4].map(circle).reduce(append, $('<ol>', { 'class': 'circles' }));
 }
 
 function circle() {
-  var $circle = $('<div>', { 'class': 'circle circle-white' });
+  var $circle = $('<li>', { 'class': 'circle circle-white' });
   $circle.on('click', function() {
-    $circle.removeClass('circle-white').addClass('circle-green')
+    $circle.removeClass('circle-white').addClass('circle-green');
   });
   return $circle;
+}
+
+function append($parent, $child) {
+  return $parent.append($child);
 }
