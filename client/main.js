6a40be40bc065e0cd0d67e21dc3084d35ce7c574
Timo Suomela <timo.suomela@reaktor.fi>
2015-01-11 18:30:32 +0200

    Each circle now has a max value. Clicking repeatedly on a circle will decrease the value by one until it reaches zero. One more click will move the circle state back to the default (empty). The background color changes according to the value in the circle, white for not set/empty, green for max value, red for all others

--- client/main.js
+++ client/main.js
@@ -1,32 +1,65 @@
-var Bacon = require('baconjs');
-var $ = require('jquery');
 require('normalize-css');
+var $ = require('jquery');
+var Bacon = require('baconjs').Bacon;
+$.fn.asEventStream = Bacon.$.asEventStream;
 
 $(document).ready(init);
 
+var program = [
+  {'name': 'Reverse', reps: 8},
+  {'name': 'Incline', reps: 8},
+  {'name': 'Single', reps: 8},
+  {'name': 'Curl', reps: 8},
+  {'name': 'Triceps', reps: 8},
+  {'name': 'Shrugs', reps: 8},
+  {'name': 'Calf', reps: 12}
+];
+
 function init() {
-  ['Reverse', 'Incline', 'Single', 'Curl', 'Triceps', 'Shrugs', 'Calf']
+  program
     .map(block)
     .reduce(append, $('<div>', { 'class': 'content' }))
     .appendTo($('body'));
 }
 
-function block(title) {
-  return $('<h2>').text(title).add(circles());
+function block(excercise) {
+  return $('<h2>')
+    .text(excercise.name)
+    .add(circles(excercise.reps));
 }
 
-function circles() {
-  return [1, 2, 3, 4].map(circle).reduce(append, $('<ol>', { 'class': 'circles' }));
+function circles(reps) {
+  return [1, 2, 3, 4].map(circle(reps)).reduce(append, $('<ol>', { 'class': 'circles' }));
 }
 
-function circle() {
-  var $circle = $('<li>', { 'class': 'circle circle-white' });
-  $circle.on('click', function() {
-    $circle.removeClass('circle-white').addClass('circle-green');
-  });
-  return $circle;
+function circle(reps) {
+  return function() {
+    var $circle = $('<li>', { 'class': 'circle circle-white' }).text('-1');
+    $circle
+      .asEventStream('click')
+      .map(1)
+      .scan(-1, substractWithWrapAround(reps))
+      .onValue(function(value) {
+        if (value == reps) {
+          $circle.removeClass('circle-white circle-red').addClass('circle-green').text(reps);
+        } else if (value >= 0) {
+          $circle.removeClass('circle-white circle-green').addClass('circle-red').text(reps);
+        } else {
+          $circle.removeClass('circle-red circle-green').addClass('circle-white').text(reps);
+        }
+        $circle.text(value);
+    });
+    return $circle;
+  };
 }
 
 function append($parent, $child) {
   return $parent.append($child);
+}
+
+function substractWithWrapAround(reps) {
+  return function (x, y) {
+    var sum = x - y;
+    return sum < -1 ? reps : sum;
+  };
 }
