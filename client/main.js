b3d6e1e37def329fdcc03cc6860570c8bf082c7b
Timo Suomela <timo.suomela@reaktor.fi>
2015-01-11 21:29:27 +0200

    Show rest timer between reps

--- client/main.js
+++ client/main.js
@@ -15,11 +15,89 @@ var program = [
   {'name': 'Calf', reps: 12}
 ];
 
+var oneAndAHalfMinuteInSeconds = 9;
+var fiveMinutesInSeconds = 5 * 60;
+var fiveSecondsInMillis = 5 * 1000;
+var timerStream = new Bacon.Bus();
+
 function init() {
-  program
+  $('body')
+    .append(timer())
+    .append(content());
+}
+
+function timer() {
+  var $timer = $('<div>', { 'class': 'timer' });
+  var counter = timerStream
+    .flatMapLatest(toCountdown);
+
+  counter
+    .map(formatCounter)
+    .doAction(setTextOf($timer))
+    .onValue(slideDown($timer));
+
+  counter
+    .filter(isZero)
+    .flatMapLatest(delay)
+    .onValue(slideUp($timer));
+
+  return $timer;
+}
+
+function delay() {
+  return Bacon.later(fiveSecondsInMillis, 1);
+}
+
+function formatCounter(value) {
+  return value > 0 ? formatTime(value) : 'BODI ON';
+}
+
+function formatTime(value) {
+  var minutes = Math.floor(value / 60);
+  var seconds = value % 60;
+  return minutes + ':' + (seconds < 10 ? '0' + seconds : seconds);
+}
+
+function isZero(x) {
+  return x == 0;
+}
+function slideDown($element) {
+  return function() {
+    $element.slideDown();
+  };
+}
+
+function slideUp($element) {
+  return function() {
+    $element.slideUp();
+  };
+}
+
+function toCountdown(seconds) {
+  return Bacon
+    .interval(1000, 1)
+    .scan(seconds, substract)
+    .takeWhile(greaterOrEqualToZero);
+}
+
+function substract(x, y) {
+  return x - y;
+}
+
+function greaterOrEqualToZero(x) {
+  return x >= 0;
+}
+
+function setTextOf($element) {
+  return function(text) {
+    $element.text(text);
+  };
+}
+
+function content() {
+  return program
     .map(block)
-    .reduce(append, $('<div>', { 'class': 'content' }))
-    .appendTo($('body'));
+    .reduce(append, $('<div>', { 'class': 'content' }));
 }
 
 function block(excercise) {
@@ -42,10 +120,13 @@ function circle(reps) {
       .onValue(function(value) {
         if (value == reps) {
           $circle.removeClass('circle-white circle-red').addClass('circle-green').text(reps);
+          countdownAfterSuccess();
         } else if (value >= 0) {
           $circle.removeClass('circle-white circle-green').addClass('circle-red').text(reps);
+          countdownAfterFailure();
         } else {
           $circle.removeClass('circle-red circle-green').addClass('circle-white').text(reps);
+          clearCountdown();
         }
         $circle.text(value);
     });
@@ -62,4 +143,20 @@ function substractWithWrapAround(reps) {
     var sum = x - y;
     return sum < -1 ? reps : sum;
   };
+}
+
+function countdownAfterSuccess() {
+  countdown(oneAndAHalfMinuteInSeconds);
+}
+
+function countdownAfterFailure() {
+  countdown(fiveMinutesInSeconds);
+}
+
+function clearCountdown() {
+  countdown(0);
+}
+
+function countdown(seconds) {
+  timerStream.push(seconds);
 }
